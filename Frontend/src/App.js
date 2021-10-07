1597de5c6fad22a3b5e7fbe9abe6d7c9b27f860a
ishi-jaiswal <ishikaj@iitbhilai.ac.in>
2021-10-07 17:41:23 +0530

    tailwind integrated

---
+++ Frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from'./components/navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <p> Hello World! </p>
+    </div>
+  );
+}
+
+export default App;
